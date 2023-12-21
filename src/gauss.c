e6cbd7d059788066bd034ae092b85a95ea63be6b
Tomasz2004 <01187580@pw.edu.pl>
2023-12-21 17:48:48 +0100

    Implementacja elementu diagonalnego

--- src/gauss.c
+++ src/gauss.c
@@ -1,20 +1,43 @@
 #include "gauss.h"
+#include "math.h"
 
 int eliminate(Matrix *mat, Matrix *b){
 	int n = mat->r;
-	for (int k = 0; k < n - 1; k++) {
-        	if (mat->data[k][k] == 0.0) {
-            		return 1; 
-        }
-        for (int i = k + 1; i < n; i++) {
-            	double m = mat->data[i][k] / mat->data[k][k];
-            	for (int j = k; j < n; j++) {
-                	mat->data[i][j] -= m * mat->data[k][j];
-            	}
-            
-            	b->data[i][0] -= m * b->data[k][0];
-        }
-    }
-    return 0;
+	for (int k=0; k<n-1; k++) {
+		int ielmax = k;
+		double elmax = fabs(mat->data[k][k]);
+		for (int i = k+1;i<n;i++) {
+			if (fabs(mat->data[i][k]) > elmax) {
+				elmax = fabs(mat->data[i][k]);
+				ielmax = i;
+			}
+		}
+		if (elmax==0) {
+			return 1;
+		}	
+		if (ielmax != k) {
+			double *temp = mat->data[k];
+			mat->data[k] = mat->data[ielmax];
+			mat->data[ielmax] = temp;
+
+			double temp_b = b->data[k][0];
+            		b->data[k][0] = b->data[ielmax][0];
+           		b->data[ielmax][0] = temp_b;
+		}
+		for (int j = k+1; j<n; j++) {
+			double r = mat->data[j][k] / mat->data[k][k];
+			for (int p = k; p<n; p++){
+				mat->data[j][p] = mat->data[j][p] - r*mat->data[k][p];
+			}
+			b->data[j][0] = b->data[j][0] - r * b->data[k][0];
+		}
+		if (mat->data[n-1][n-1] == 0) {
+			return 1;
+		}
+	}
+	if (mat->data[n-1][n-1] == 0) {
+        	return 1;	
+	}
+	return 0;
 }
 
