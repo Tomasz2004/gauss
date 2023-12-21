409e6bff4c998aef309adcba47dbb4223d5c6f86
bciupak <153275462+bciupak@users.noreply.github.com>
2023-12-21 19:00:41 +0100

    Stworzenie pliku backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,31 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 
-				return 0;
+	int n = mat->r;
+
+	if (x->c!= 1 || b->c!=1) {
+		return 2;
+
+	}
+
+	for (int i = n - 1; i >= 0; i --){
+		double s = 0;
+		for (int j = i + 1; j < n; j++){
+			s += mat->data[i][j] * (x -> data[j][0]);
+
+		}
+
+		if (mat->data[i][i])
+			return 1;
+		x->data[i][0] = (b->data[i][0] - s) / mat ->data[i][i];
+
+	}
+
+	return 0;
 }
 
 
