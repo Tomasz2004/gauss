060a70a2ae4b87f862d59b230df55eac4e2f406d
Tomasz2004 <01187580@pw.edu.pl>
2023-12-21 14:32:05 +0100

    Implementacja eliminacji Gaussa bez elementu diagonalnego

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,20 @@
 #include "gauss.h"
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+	int n = mat->r;
+	for (int k = 0; k < n - 1; k++) {
+        	if (mat->data[k][k] == 0.0) {
+            		return 1; 
+        }
+        for (int i = k + 1; i < n; i++) {
+            	double m = mat->data[i][k] / mat->data[k][k];
+            	for (int j = k; j < n; j++) {
+                	mat->data[i][j] -= m * mat->data[k][j];
+            	}
+            
+            	b->data[i][0] -= m * b->data[k][0];
+        }
+    }
+    return 0;
 }
 
